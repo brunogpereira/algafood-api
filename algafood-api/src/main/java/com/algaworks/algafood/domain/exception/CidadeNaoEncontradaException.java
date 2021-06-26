83d77e491d03c301402c7d0b6ed793fc151313bc
Bruno Gomes <brunogpereira@hotmail.com>
2021-06-26 17:13:41 -0300

    Tratamento e modelagem de erros API. (Exception de granularidade fina)

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/CidadeNaoEncontradaException.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class CidadeNaoEncontradaException extends EntidadeNaoEncontradaException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CidadeNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public CidadeNaoEncontradaException(Long cidadeId) {
+		this(String.format("Não existe um cadastro de estado com código %d", cidadeId));
+	}
+
+	
+	
+}
+
---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/RestauranteNaoEncontradoException.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class RestauranteNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RestauranteNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public RestauranteNaoEncontradoException(Long restauranteId) {
+		this(String.format("Não existe um cadastro de estado com código %d", restauranteId));
+	}
+
+	
+	
+}
+
